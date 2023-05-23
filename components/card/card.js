32fd3ce8d76824581cf6cd3b2a3f64bddac7eb61
muhibzeon <muib.mohaimen1994@gmail.com>
2023-05-23 20:02:19 +0200

    wip: working with sections

--- components/card/card.js
+++ components/card/card.js
@@ -1,11 +1,47 @@
 import Image from "next/image";
+import styles from "./card.module.css";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import cls from "classnames";
 
 const Card = (props) => {
-  const { imgUrl, size } = props;
+  const {
+    imgUrl = "https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80",
+    size,
+    id,
+  } = props;
+
+  const [imgSrc, setImgSrc] = useState(imgUrl);
+
+  const classMap = {
+    large: styles.lgItem,
+    medium: styles.mdItem,
+    small: styles.smItem,
+  };
+
+  const handleOnError = () => {
+    console.log("Hi Error");
+    setImgSrc(
+      "https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80"
+    );
+  };
+
+  const scale = id === 0 ? { scaleY: 1.1 } : { scale: 1.1 };
+
   return (
-    <div>
-      Card
-      <Image src={imgUrl} alt="image" width={300} height={300} />
+    <div className={styles.container}>
+      <motion.div
+        className={cls(styles.imgMotionWrapper, classMap[size])}
+        whileHover={{ ...scale }}
+      >
+        <Image
+          src={imgSrc}
+          alt="image"
+          fill={true}
+          onError={handleOnError}
+          className={styles.cardImg}
+        />
+      </motion.div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,12 @@ import styles from "../styles/Home.module.css";
 import Banner from "../components/banner/banner";
 import NavBar from "../components/nav/navbar";
 import Card from "../components/card/card";
+import SectionCards from "../components/card/section-cards";
+import { getVideos } from "../lib/videos";
 
 export default function Home() {
+  const disneyVideos = getVideos();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -21,9 +25,10 @@ export default function Home() {
         imgUrl="/static/stranger.png"
       />
 
-      <Card imgUrl="/static/stranger.png" size="large" />
-      <Card imgUrl="/static/stranger.png" size="medium" />
-      <Card imgUrl="/static/stranger.png" size="small" />
+      <div className={styles.sectionWrapper}>
+        <SectionCards title="Disney" videos={disneyVideos} size="large" />
+        <SectionCards title="Disney" videos={disneyVideos} size="medium" />
+      </div>
     </div>
   );
 }
